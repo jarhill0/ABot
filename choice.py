8722b630eaef9161aac42da4581b76756f4dca9c
jarhill0 <jarhill0@gmail.com>
2017-05-23 15:39:22 -0700

    fixed bug with /choice that could cause it to crash the script

--- choice.py
+++ choice.py
@@ -1,14 +1,11 @@
 import random
 
 
-def choice(items):
-    items = items.split(';')
-
-    while '' in items:
-        items.remove('')
+def choice(string):
+    items = string.split(';')
 
     return random.choice(items).strip()
 
 
 if __name__ == '__main__':
-    print(choice('Thing 1;Thing 2; Thing 3'))
+    print(choice(';;;'))
