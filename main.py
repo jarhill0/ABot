40b992f5df6cc5b0648007606b7acd722c0ae5e5
jarhill0 <jarhill0@gmail.com>
2017-06-05 14:32:41 -0700

    code cleanup to remove naming conflicts and other errors

--- main.py
+++ main.py
@@ -47,6 +47,7 @@ def main():
                 tg.send_photo(new_comic[0])
                 tg.send_message(new_comic[1])
 
+
 # Dict to store the commands of the bot
 bot_commands = {}
 
@@ -81,25 +82,25 @@ def start(message):
                    "\n/wa - Follow with a query to get information from WolframAlpha" \
                    "\n/nextlaunch - Get information on the next SpaceX launch"
     data = {'chat_id': current_chat,
-            'text': bot_message,}
+            'text': bot_message, }
     tg.send_message(data)
 
 
 bot_commands["/start"] = start
 
 
-def help(message):
+def help_(message):
     current_chat = message['chat']['id']
     data = {'chat_id': current_chat,
             'text': "Commands:\n"
                     "\n/redditposts [subreddit] - List 5 hot posts from /r/[subreddit]"
                     "\n/reddit [shortlink] - Get info on the linked Reddit post."
                     "\n/wa - Follow with a query to get information from WolframAlpha"
-                    "\n/nextlaunch - Get information on the next SpaceX launch",}
+                    "\n/nextlaunch - Get information on the next SpaceX launch", }
     tg.send_message(data)
 
 
-bot_commands["/help"] = help
+bot_commands["/help"] = help_
 
 
 def settings(message):
@@ -115,7 +116,7 @@ bot_commands["/settings"] = settings
 def shrug(message):
     current_chat = message['chat']['id']
     data = {'chat_id': current_chat,
-            'text': '¯\_(ツ)_/¯',}
+            'text': '¯\_(ツ)_/¯', }
     tg.send_message(data)
 
 
@@ -132,12 +133,15 @@ def redditposts(message):
     except IndexError:
         bot_message = 'Specify a subreddit after /redditposts (e.g. /redditposts funny)'
     finally:
+        # noinspection PyUnboundLocalVariable
         data = {'chat_id': current_chat,
                 'text': bot_message,
                 'disable_web_page_preview': True}
         tg.send_message(data)
         try:
+            # noinspection PyUnboundLocalVariable
             if posts_dict is not None:
+                # noinspection PyUnboundLocalVariable
                 reddit.add_posts_to_dict(current_chat, posts_dict)
         except NameError:
             pass
@@ -147,25 +151,22 @@ bot_commands["/redditposts"] = redditposts
 
 
 def stop(message):
-    current_chat = message['chat']['id']
     username = message['from'].get('username', None)
     if (username == config.owner_un or username in config.owner_uns) and time.time() - \
             message[
                 'date'] < 15:
         sys.exit()
 
-        
+
 bot_commands["/stop"] = stop
 
 
-def launch(message):
+def launch_(message):
     current_chat = message['chat']['id']
     send_launch_message(next_launch, current_chat)
 
 
-bot_commands["/nextlaunch"] = launch
-
-
+bot_commands["/nextlaunch"] = launch_
 
 
 def secretcommand(message):
@@ -187,6 +188,7 @@ def wa(message):
     if command_block == '':
         bot_message = 'Specify a query after /wa (e.g. /wa calories in a bagel)'
     else:
+        # noinspection PyBroadException
         try:
             bot_message = query_wa(command_block)
         except:
@@ -257,7 +259,7 @@ def frog(message):
     current_chat = message['chat']['id']
     image_url = rand_frog.main()
     data = {'chat_id': current_chat,
-            'photo': image_url,}
+            'photo': image_url, }
     tg.send_photo(data)
 
 
@@ -278,7 +280,7 @@ def choices(message):
     else:
         data = {'chat_id': current_chat,
                 'text': choice.choice(command_block) + '\n\n(chosen for ' + user_name(
-                    message['from']) + ')',}
+                    message['from']) + ')', }
     print(data)
     tg.send_message(data)
 
@@ -314,8 +316,6 @@ def handle(response):
 
             message_text = message.get('text', None)
             current_chat = message['chat']['id']
-            chat_type = message['chat']['type']
-            orig_message_id = message['message_id']
 
             if 'entities' in message.keys():
                 entities = message['entities']
@@ -367,7 +367,7 @@ def send_launch_message(launch, chat_id):
     data = {'chat_id': chat_id,
             'text': message,
             'parse_mode': 'Markdown',
-            'disable_web_page_preview': True,}
+            'disable_web_page_preview': True, }
     tg.send_message(data)
 
 
