6b2f63d7000017e497fe657ee2546cb717dea86a
jarhill0 <jarhill0@gmail.com>
2017-06-18 12:13:21 -0700

    bugfix in /myscore

--- scores.py
+++ scores.py
@@ -12,6 +12,7 @@ def get_score(id_):
 
 
 def change_score(id_, change):
+    get_score(id_)  # ensure that a score exists
     scores[id_] += change
     with open(os.path.join(helpers.folder_path(), 'data', 'scores.json'), 'w') as f:
         json.dump(scores, f)
