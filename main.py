92f4841f34c0eae2c379043d3d72490c7626b307
jarhill0 <jarhill0@gmail.com>
2017-06-25 22:52:59 -0700

    handle spaces more effectively in regex

--- main.py
+++ main.py
@@ -139,7 +139,7 @@ def xkcd_command(message):
     """View latest xkcd comic, or view comic specified by number."""
     current_chat = message['chat']['id']
     message_text = message.get('text', None).lower()
-    xkcd_regex = re.compile(r'/xkcd(?:@a_group_bot)?\s?(\d+|rand(?:om)?)?(?:\s|$)', re.I)
+    xkcd_regex = re.compile(r'/xkcd(?:@a_group_bot)?(?:\s(\d+|rand(?:om)?))?(?:\s|$)', re.I)
     command_opt = xkcd_regex.search(message_text).group(1)
 
     if command_opt is None:
@@ -170,7 +170,7 @@ def redditlimit(message):
     """Set limit for /redditposts."""
     from_id = message['from']['id']  # respond always in PM
     message_text = message.get('text', None).lower()
-    limit_regex = re.compile(r'/redditlimit(?:@a_group_bot)?\s?(\d+)?(?:\s|$)', re.I)
+    limit_regex = re.compile(r'/redditlimit(?:@a_group_bot)?(?:\s(\d+))?(?:\s|$)', re.I)
     command_opt = limit_regex.search(message_text).group(1)
 
     if command_opt is None:
