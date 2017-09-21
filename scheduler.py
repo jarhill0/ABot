4f06be38b5f0f2982168b73d53437408f36dbf12
jarhill0 <jarhill0@gmail.com>
2017-09-21 10:16:22 -0700

    ✨ Add Scheduler class for refactor of event scheduling

---
+++ scheduler.py
@@ -0,0 +1,51 @@
+import time
+
+
+class Scheduler:
+    """Schedules events to happen at a specific time. Events scheduled in the past will happen immediately"""
+
+    def __init__(self):
+        self.events = dict()
+
+    def __repr__(self):
+        text = ['Scheduler with {} events.'.format(len(self.events))]
+        for key, event in self.events.keys(), self.events.values():
+            text.append('{}: {}'.format(key, event))
+        return '\n'.join(text)
+
+    def add_event(self, event_time, func, args=None, kwargs=None, force=False):
+        event = Event(func, args if args else [], kwargs if kwargs else {})
+        if event_time < time.time():
+            # Event is scheduled in the class
+            event.execute()
+            return True
+        if event_time not in self.events.keys() or force:
+            self.events[event_time] = event
+            return True
+        return False  # failed because an event already exists at that time
+
+    def check_events(self):
+        print('checking')
+        current_time = time.time()
+        executed = []
+        for event_time in self.events.keys():
+            if event_time < current_time:
+                self.events[event_time].execute()
+                executed.append(event_time)
+        for time_ in executed:
+            del self.events[time_]
+
+
+class Event:
+    """A representation of a function to execute and its argument."""
+
+    def __init__(self, func, args, kwargs):
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+
+    def __repr__(self):
+        return '{}(args={}, kwargs={})'.format(self.func.__name__, self.args, self.kwargs)
+
+    def execute(self):
+        self.func(*self.args, **self.kwargs)
