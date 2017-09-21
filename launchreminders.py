5b621f88f340542c27c703bb39737b193df5cdde
jarhill0 <jarhill0@gmail.com>
2017-09-21 11:07:22 -0700

    🐛 When we schedule launch reminders within 24h of the launch, don't spam messages for the past.

--- launchreminders.py
+++ launchreminders.py
@@ -23,5 +23,5 @@ def set_launch_triggers(launch, schedule):
     start_time = launch['wsstamp']
     alert_times = [start_time, start_time - (60 * 60), start_time - (60 * 60 * 4), start_time - (60 * 60 * 24)]
     for alert_time in alert_times:
-        schedule.add_event(alert_time, send_automated_message, args=[launch])
+        schedule.add_event(alert_time, send_automated_message, args=[launch], execute_past=False)
     schedule.add_event(start_time + 60 * 60, schedule_launches, args=[schedule])  # refresh after a launch has started
