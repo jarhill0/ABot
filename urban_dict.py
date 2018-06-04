e5c0cff255c6eae1ef22c2b9bd8152e1a2b30967
Joe RH <jarhill0@users.noreply.github.com>
2018-06-03 17:05:49 -0700

    .json() lol

--- urban_dict.py
+++ urban_dict.py
@@ -1,12 +1,10 @@
-import json
-
 import requests
 
 API_PATH = 'http://api.urbandictionary.com/v0/define'
 
 
 def build_message(term):
-    ud_response = json.loads(requests.get(API_PATH, params={'term': term}).content.decode('utf-8'))
+    ud_response = requests.get(API_PATH, params={'term': term}).json()
     if ud_response['result_type'] != 'exact':  # unknown how many possible responses the API gives here
         return 'No definition found for {}.'.format(term)
     entry = ud_response['list'][0]
