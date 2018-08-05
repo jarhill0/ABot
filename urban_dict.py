ef121f5675efdc94403860d9cd26aa8f825b63ef
jarhill0 <jarhill0@users.noreply.github.com>
2018-08-05 15:19:46 -0700

    UD API changed; actually respond when no definition found

--- urban_dict.py
+++ urban_dict.py
@@ -6,8 +6,9 @@ API_PATH = 'http://api.urbandictionary.com/v0/define'
 
 def send_message(chat, term, index=0):
     ud_response = requests.get(API_PATH, params={'term': term}).json()
-    if ud_response['result_type'] != 'exact':  # unknown how many possible responses the API gives here
-        return 'No definition found for {}.'.format(term)
+    if len(ud_response['list']) == 0:
+        chat.send_message('No definition found for {}.'.format(term))
+        return
     index = 0 if index >= len(ud_response['list']) else index
     entry = ud_response['list'][index]
     word = entry['word']
