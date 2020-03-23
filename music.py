89ece39c6b6f2ae05a18b2bd795cb0b40085a9d8
fydai <fydai@berkeley.edu>
2020-03-22 23:30:32 -0700

    Change telegramusic url in music.py

--- music.py
+++ music.py
@@ -2,7 +2,7 @@ import requests
 
 
 def add(message, text):
-    """Follow with a link to add to https://telegramusic.ml."""
+    """Follow with a link to add to https://telegramusic.appspot.com."""
     words = text.partition(' ')[2].split()
     if not words:
         message.reply('Please provide a link.')
@@ -11,10 +11,10 @@ def add(message, text):
     user = message.user
     name = user.full_name if user else 'a friend'  # message might not have a user
     link = words[0]
-    response = requests.post('https://telegramusic.ml/add/',
+    response = requests.post('https://telegramusic.appspot.com/add/',
                              data={'url': link, 'name': name})
     if response.status_code == 200:
         location = response.json()['link']
-        message.reply('Your song is live at https://telegramusic.ml{}'.format(location))
+        message.reply('Your song is live at https://telegramusic.appspot.com{}'.format(location))
     else:
         message.reply('Could not add that link. Try another?')
